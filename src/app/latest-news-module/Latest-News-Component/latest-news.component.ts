e8243a387fe9c547d82b3d86e5cef445f6415638
Naveen Jain <47914199+JainNaveen94@users.noreply.github.com>
2020-05-13 04:50:32 +0530

    Designing complete

--- src/app/latest-news-module/Latest-News-Component/latest-news.component.ts
+++ src/app/latest-news-module/Latest-News-Component/latest-news.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { INews } from 'src/app/shared/interfaces/INews.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-latest-news',
   templateUrl: './latest-news.component.html',
-  styleUrls: ['./latest-news.component.styl']
+  styleUrls: ['./latest-news.component.styl'],
 })
 export class LatestNewsComponent implements OnInit {
+  newsInfo: INews[] = [];
 
-  constructor() { }
+  constructor(private route: Router) {}
 
-  ngOnInit(): void {
+  moreInfo(uri:string) {
+    window.open(uri, '_blank')
   }
 
+  ngOnInit(): void {}
 }
